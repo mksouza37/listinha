a3631ab6d69185bef7f9994115a2a4812f0f2d5d
mksouza37 <markusn37@gmail.com>
2025-08-01 10:41:15 -0300

    Fix 6

--- firebase.py
+++ firebase.py
@@ -1,4 +1,11 @@
-from firebase_admin import firestore
+import os
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+if not firebase_admin._apps:
+    cred_path = os.getenv("FIREBASE_CREDENTIALS")
+    cred = credentials.Certificate(cred_path)
+    firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
