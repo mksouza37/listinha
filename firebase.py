71b3ac5bd41934538e51e4a42842659b9c280194
mksouza37 <markusn37@gmail.com>
2025-08-01 10:48:26 -0300

    Fix 7

--- firebase.py
+++ firebase.py
@@ -1,12 +1,13 @@
 import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-if not firebase_admin._apps:
-    cred_path = os.getenv("FIREBASE_CREDENTIALS")
-    cred = credentials.Certificate(cred_path)
-    firebase_admin.initialize_app(cred)
+# Parse JSON string from env
+firebase_creds = json.loads(os.getenv("FIREBASE_CREDENTIALS"))
+cred = credentials.Certificate(firebase_creds)
 
+firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def add_item(phone, item):
