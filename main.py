9937555dcb35335ab653b7cae3399a5858b628a3
mksouza37 <markusn37@gmail.com>
2025-08-08 15:09:54 -0300

    Fix 87

--- main.py
+++ main.py
@@ -406,7 +406,6 @@ async def whatsapp_webhook(request: Request):
             send_message(from_number, LIST_EMPTY_PDF)
         else:
             timestamp = int(time.time())
-            #pdf_url = f"https://listinha-t5ga.onrender.com/view?g={doc_id}&format=pdf&footer=true&download=true&t={timestamp}"
             pdf_url = f"https://listinha-t5ga.onrender.com/view?g={doc_id}&format=pdf&footer=true&&t={timestamp}"
             send_message(from_number, list_download_url(pdf_url))
 
@@ -419,7 +418,7 @@ async def whatsapp_webhook(request: Request):
         doc_id = quote(raw_doc_id, safe="")
         timestamp = int(time.time())
 
-        pdf_url = f"https://listinha-t5ga.onrender.com/view?g={doc_id}&format=pdf&mode=vc&footer=true&download=true&t={timestamp}"
+        pdf_url = f"https://listinha-t5ga.onrender.com/view?g={doc_id}&format=pdf&mode=vc&footer=true&t={timestamp}"
         send_message(from_number, list_detailed_url(pdf_url))
         return {"status": "ok"}
 
